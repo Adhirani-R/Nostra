beadeb3e270f9c0edaf7a393b26ac122747aaa91
Adhirani-R <154663878+Adhirani-R@users.noreply.github.com>
2024-01-15 23:40:25 +0530

    Update collections.js

--- collections.js
+++ collections.js
@@ -22,52 +22,52 @@ closenav.addEventListener("click", function () {
 
 //Collections
 
-//Filters
+// // //Filters
 
-var check = document.getElementsByName("check")
-var container = document.querySelector(".product__pics")
-var  divlist= container.querySelectorAll("div")
-var summer = container.getElementById("summer")
-
-
-// skill = []
-
-for (count = 0; count < check.length; count++) {
-   check[count].addEventListener("click", function () {
-      if (event.target.checked) {
-         console.log(event.target.value)
-         // skill.push(event.target.value)
-         var enteredvalue = event.target.value
-
-         if (divlist.textContent==enteredvalue) {
-            divlist.style.display = "none"
-         }
-         else {
-            divlist.style.display = "block"
-         }
+// // var check = document.getElementsByName("check")
+// // var container = document.querySelector(".product__pics")
+// // var  divlist= container.querySelectorAll("div")
+// // var summer = container.getElementById("summer")
 
 
-      }
-      // else {
-      //    console.log("unchecked")
-      //    //find index
-      //    var index = skill.indexOf(event.target.value)
-      //    // skill.splice(index, 1)
-      //    enteredvalue = event.target.checked
-      //    for (i = 0; i < h2list.length; i++) {
-      //       if (h2list[i].textContent.indexOf(enteredvalue) < 0) {
-      //          h2list[i].style.display = "none"
-      //       }
-      //       else {
-      //          h2list[i].style.display = "block"
-      //       }
-      //    }
-      // }
+// // skill = []
 
-   }
-   )
+// for (count = 0; count < check.length; count++) {
+//    check[count].addEventListener("click", function () {
+//       if (event.target.checked) {
+//          console.log(event.target.value)
+//          // skill.push(event.target.value)
+//          var enteredvalue = event.target.value
 
-}
+//          if (divlist.textContent==enteredvalue) {
+//             divlist.style.display = "none"
+//          }
+//          else {
+//             divlist.style.display = "block"
+//          }
+
+
+//       }
+//       // else {
+//       //    console.log("unchecked")
+//       //    //find index
+//       //    var index = skill.indexOf(event.target.value)
+//       //    // skill.splice(index, 1)
+//       //    enteredvalue = event.target.checked
+//       //    for (i = 0; i < h2list.length; i++) {
+//       //       if (h2list[i].textContent.indexOf(enteredvalue) < 0) {
+//       //          h2list[i].style.display = "none"
+//       //       }
+//       //       else {
+//       //          h2list[i].style.display = "block"
+//       //       }
+//       //    }
+//       // }
+
+//    }
+//    )
+
+// }
 
 
 
